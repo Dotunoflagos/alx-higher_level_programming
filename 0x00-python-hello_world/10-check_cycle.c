eb45e8eb68921b807e0680a1f92b7bdcde666a1c
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-25 22:08:10 +0100

    c test 1

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,30 @@
 #include "lists.h"
-
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * check_cycle -linked list cycle
+ * @list: the pointer to the struct
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *current;
-    listint_t *current2;
+	listint_t *current = list;
+	listint_t *current2 = list;
 
-    current = list;
-    while (current != NULL)
-    {
-        if (current == current2)
-            return 1;
-        current = current->next;
-        current2 = current->next;
-    }
-            return 0;
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	current2 = current2->next;
+
+	while (current != NULL && current2 != NULL && current2->next != NULL)
+	{
+		if (current2 == current)
+		{
+			return (1);
+		}
+		current2 = current2->next->next;
+		current = current->next;
+	}
+	return (0);
 }
