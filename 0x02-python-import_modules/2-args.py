4cb052e913e8664e9906f209232b5002cf37939f
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-26 16:27:53 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,7 @@ if __name__ == '__main__':
     inputno = len(argv)
     if inputno == 1:
         print(f'{inputno - 1} arguments.')
-    else:
+    elif len(argv) > 2:
         print(f'{inputno - 1} arguments:')
         for i in range(1,inputno):
             print(f"{i}: {argv[i]}")
-        argv[i]
