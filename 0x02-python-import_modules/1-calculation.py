3b2c8681e5a08455d7c7babe9c625a50a353bd1d
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-26 16:21:20 +0100

    2-args.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,8 +3,7 @@ if __name__ == '__main__':
     import calculator_1 as c
     a = 10
     b = 5
-    print(f"{a} + {b} = {c.add(a, b)}")
-    print(f"{a} - {b} = {c.sub(a, b)}")
-    print(f"{a} * {b} = {c.mul(a, b)}")
-    print(f"{a} / {b} = {c.div(a, b)}")
-
+print('{:d} + {:d} = {:d}'.format(a, b, c.add(a, b)))
+print('{:d} - {:d} = {:d}'.format(a, b, c.sub(a, b)))
+print('{:d} * {:d} = {:d}'.format(a, b, c.mul(a, b)))
+print('{:d} / {:d} = {:d}'.format(a, b, c.div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-inputno = len(argv)
-if inputno == 1:
-    print(f'{inputno - 1} arguments.')
-else:
-    print(f'{inputno - 1} arguments:')
-    for i in range(1,inputno):
-        print(f"{i}: {argv[i]}")
-    argv[i]
+if __name__ == '__main__':
+    from sys import argv
+    inputno = len(argv)
+    if inputno == 1:
+        print(f'{inputno - 1} arguments.')
+    else:
+        print(f'{inputno - 1} arguments:')
+        for i in range(1,inputno):
+            print(f"{i}: {argv[i]}")
+        argv[i]
