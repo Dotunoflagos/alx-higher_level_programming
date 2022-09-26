7157c178c35d25373df32bcd04d93eb19a4856ef
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-26 16:37:42 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+from this import d
+
+
 if __name__ == '__main__':
     from sys import argv
     inputno = len(argv)
     if inputno == 1:
         print(f'{inputno - 1:d} arguments.')
-    else:
-        print(f'{inputno - 1} arguments:')
-        for i in range(1,inputno):
-            print(f"{i:d}: {argv[i]:s}")
+    elif inputno == 2:
+        print('1 arguments:')
+        print(f"1: {argv[1]:s}")
+    elif inputno > 2:
+            print(f'{inputno - 1:d} arguments:')
+            for i in range(1,inputno):
+                print(f"{i:d}: {argv[i]:s}")
