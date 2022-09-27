cfcf25560cab59ab40655bb6baefbb3a6a7e8248
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-27 03:36:12 +0100

    New string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in my_string:
+        if my_string is None:
+            pass
+        elif i != "c" and i != "C":
+            news = news + i
+        return news
