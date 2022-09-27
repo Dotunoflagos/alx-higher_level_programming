791a9a5bd60eb8735e8f755505101e74a2b44c66
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-27 03:23:29 +0100

    Returns acopy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     list_my = my_list
     if (idx < 0):
-        return my_list
+        return list_my
     elif (idx > len(my_list) - 1):
-        return my_list
+        return list_my
     else:
         list_my[idx] = element
         return list_my
