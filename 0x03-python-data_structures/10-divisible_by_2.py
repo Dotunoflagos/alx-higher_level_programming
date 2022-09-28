56aa74ff2dab3a0cef698239ac734bcffc09fa20
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-28 09:54:40 +0100

    Is divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    answer = []
-    j = 0
+    answer, i = [], 0
     if my_list:
-        for i in my_list:
-            if i % 2 == 0:
-                answer[j] = 'True'
-                j += 1
-            else:
-                answer[j] = 'False'
-                j += 1
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                answer.append(True)
+            elif my_list[i] % 2 == 1:
+                answer.append(False)
+        return answer
