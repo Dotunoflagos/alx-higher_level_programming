c55a4a49da8ac98752852a17105591ecf9d4c442
dotunoflagos <adedotun_balogun@yahoo.com>
2022-09-30 17:19:34 +0100

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    i = 0
+    final = my_list[:]
+    while i < len(my_list):
+        if final[i] == search:
+            final[i] = replace
+    return final
