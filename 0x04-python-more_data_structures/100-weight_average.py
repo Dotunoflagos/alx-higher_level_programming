d5e0ecc1ccc77942bfee780deca5597ef7dbc7fb
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-13 03:13:36 +0100

    weighted avrage

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,14 +3,15 @@ def weight_average(my_list=[]):
     add = 0
     add2 = 0
     mul = 1
+
     for i in my_list:
         del mul
         mul = 1
         for j in i:
             mul *= j
         add += mul
-        
+
     for i in my_list:
         add2 += i[1]
-        
+
     return(add/add2)
