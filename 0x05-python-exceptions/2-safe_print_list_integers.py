36c7209ac14ac61dd3278491b4bca99b97cd2bb3
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-16 23:35:46 +0100

    Print num

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        j = 0
+        k = 0
+        for a in my_list[:x]:
+            try:
+                print("{:d}".format(my_list[j]), end="")
+                j += 1
+                k += 1
+            except (ValueError, TypeError):
+                j += 1
+        print("\n", end="")
+        return k
+    except (ValueError, TypeError):
+        for a in my_list[j:]:
+            try:
+                print("{:d}".format(my_list[j]), end="")
+                j += 1
+                k += 1
+            except (ValueError, TypeError):
+                j += 1
+        print("\n", end="")
+        return k
+    except IndexError:
+        raise
