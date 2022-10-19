c3e0158d7f6b2272d4f549da01f66559760ad19e
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-19 03:46:08 +0100

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Assign the size variable"""
+    def __init__(self, size=0):
+        try:
+            if isinstance(size, int) and size >= 0:
+                self.__size = size
+                self.size = size
+            elif isinstance(size, int) and size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
+
+    """Returnes area of square"""
+    def area(self):
+        try:
+            if isinstance(self.size, int) and self.size >= 0:
+                return self.size ** 2
+            elif isinstance(self.size, int) and self.size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
+    
+    """Returnes size of square"""
+    def size(self):
+        return self.size
+    
+    """Changes values """
+    def size(self, value):
+        try:
+            if isinstance(value, int) and value >= 0:
+                self.size = value
+            elif isinstance(value, int) and value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
