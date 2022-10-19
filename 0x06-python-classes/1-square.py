d8862220967fd38fbdf48fb4e15bdb4bc967716e
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-19 02:29:59 +0100

    new class with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty class"""
+
+
+class Square:
+    """Empty class"""
+    def __init__(self, size):
+        self.size = size
+    def square():
+        """Empty class"""
