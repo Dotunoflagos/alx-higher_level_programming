39b370fd4ae4004380a50c4ce6a7d3a4ad98eb47
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-19 04:55:18 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,20 +8,8 @@ class Square:
         try:
             if isinstance(size, int) and size >= 0:
                 self.__size = size
-                self.size = size
+                #self.size = size
             elif isinstance(size, int) and size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                raise TypeError('size must be an integer')
-        except Exception:
-            raise
-
-    """Returnes area of square"""
-    def area(self):
-        try:
-            if isinstance(self.size, int) and self.size >= 0:
-                return self.size ** 2
-            elif isinstance(self.size, int) and self.size < 0:
                 raise ValueError('size must be >= 0')
             else:
                 raise TypeError('size must be an integer')
@@ -29,15 +17,29 @@ class Square:
             raise
     
     """Returnes size of square"""
+    @property
     def size(self):
-        return self.size
+        return self.__size
     
     """Changes values """
+    @size.setter
     def size(self, value):
         try:
             if isinstance(value, int) and value >= 0:
-                self.size = value
+                self.__size = value
             elif isinstance(value, int) and value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
+    
+    """Returnes area of square"""
+    def area(self):
+        try:
+            if isinstance(self.size, int) and self.size >= 0:
+                return self.size ** 2
+            elif isinstance(self.size, int) and self.size < 0:
                 raise ValueError('size must be >= 0')
             else:
                 raise TypeError('size must be an integer')
