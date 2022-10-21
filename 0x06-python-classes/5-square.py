95b00c5a69e1b1637dab997bbd3ffae0b93b5a10
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-21 02:12:27 +0100

    Print us

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -58,18 +58,3 @@ class Square:
             
             print('\n', end='')
             i += 1
-
-my_square = Square(3)
-my_square.my_print()
-
-print("--")
-
-my_square.size = 10
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
