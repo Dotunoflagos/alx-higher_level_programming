1c6ca9babe3d5a47db3f9db7d9155c2df7c5cb33
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-21 01:41:07 +0100

    print sq

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Assign the size variable"""
+    def __init__(self, size=0):
+        try:
+            if isinstance(size, int) and size >= 0:
+                self.__size = size
+            elif isinstance(size, int) and size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
+
+    """Returnes size of square"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Changes values """
+    @size.setter
+    def size(self, value):
+        try:
+            if isinstance(value, int) and value >= 0:
+                self.__size = value
+            elif isinstance(value, int) and value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
+
+    """Returnes area of square"""
+    def area(self):
+        try:
+            if isinstance(self.size, int) and self.size >= 0:
+                return self.size ** 2
+            elif isinstance(self.size, int) and self.size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except Exception:
+            raise
+
+    def my_print(self):
+        len = self.size
+        i, j = 0
+        while i < len:
+            while j < len:
+                print("Exception: {}".format('#'))
+                j += 1
+            i += 1
