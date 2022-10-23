5c761ad760ca28a8ed809cb1c310b41ec86af3ed
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-23 22:37:43 +0100

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,16 +10,18 @@ class Rectangle:
         elif name == "height":
             str = "height"
 
-        if not isinstance(value, int):
-            raise TypeError("{} must be an intiger".format(str))
-        elif value < 0:
-            raise ValueError("{} must be >= 0".format(str))
-        else:
-            if name == "width":
-                self.__weidth = value
-            elif name == "height":
-                self.__height = value
-
+        try:
+            if not isinstance(value, int):
+                raise TypeError("{} must be an intiger".format(str))
+            elif value < 0:
+                raise ValueError("{} must be >= 0".format(str))
+            else:
+                if name == "width":
+                    self.__weidth = value
+                elif name == "height":
+                    self.__height = value
+        except Exception:
+            raise
 
     def __init__(self, width=0, height=0):
         self.__st(width, "width")
