c738a1b2a21f4d296253bfc9133986fac9ae4464
dotunoflagos <adedotun_balogun@yahoo.com>
2022-10-23 22:06:37 +0100

    1-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 """Creat rectangle class"""
-
 
 class Rectangle:
     """Rectangle class"""
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Creat rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __st(self, value, name=""):
+        if name == "width":
+            str = "width"
+        elif name == "height":
+            str = "height"
+
+        try:
+            if not isinstance(value, int):
+                raise TypeError("{} must be an intiger".format(str))
+            elif value < 0:
+                raise ValueError("{} must be >= 0".format(str))
+            else:
+                if name == "width":
+                    self.__weidth = value
+                elif name == "height":
+                    self.__height = value
+        except Exception as e:
+            print(e)
+
+    def __init__(self, width=0, height=0):
+        self.__st(width, "width")
+        self.__st(height, "height")
+
+    @property
+    def width(self):
+        return self.__weidth
+
+    @width.setter
+    def width(self, value):
+        self.__st(value, "width")
+
+    @property
+    def height(self):
+        return self.__weidth
+
+    @height.setter
+    def height(self, value):
+        self.__st(value, "height")
