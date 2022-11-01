88404bdf82faf62e1fd5ea5457988d2c3440c0d6
dotunoflagos <adedotun_balogun@yahoo.com>
2022-11-01 19:05:33 +0100

    more teste

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,14 @@ def add_integer(a, b=98):
     Check if the parameters are float, if so, casted it to integer.
     If parameters are integer or float), raise an exception with a message. """
     def chk(aa, ab):
-        if type(aa) not in [int, float]:
+        if not isinstance(aa, (int, float)):
             raise TypeError('{} must be an integer'.format(ab))
-    
+
     if type(a) == float:
         a = int(a)
     if type(b) == float:
         b = int(b)
-        
+
     chk(a, 'a')
     chk(b, 'b')
     return a + b
