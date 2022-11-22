adda7d443ff758db297dcb4eb2a1cbe991692706
dotunoflagos <adedotun_balogun@yahoo.com>
2022-11-22 15:33:32 +0100

    list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,10 @@ Creates a class inheriting from the list class.
 
 class MyList(list):
     """Inherits from list"""
-    
+ 
     def print_sorted(self):
         """prints in ascending order"""
-    
+
         new_list = self[:]
         new_list.sort()
         print("{}".format(new_list))
