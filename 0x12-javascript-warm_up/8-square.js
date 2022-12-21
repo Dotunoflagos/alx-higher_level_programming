123843d56ae906408ecf40ff3dd8f930d407d0ef
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-21 01:25:18 +0100

    new js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const value = Number(process.argv[2]);
 if (value) {
   for (let i = value; i > 0; i--) {
     for (let j = value; j > 0; j--) {
-      process.stdout.write('x');
+      process.stdout.write('X');
     }
     console.log('');
   }
