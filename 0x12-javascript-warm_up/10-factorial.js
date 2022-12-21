00028e7490b7d289cc9464ea6e4bfece94d83bca
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-21 02:11:13 +0100

    recursion

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let value = Number(process.argv[2]);
+let total = 1
+
+function add (a) {
+  if (!a) {
+    return;
+  }
+  total = total * a
+  add (a - 1);
+}
+
+add(value);
+console.log(total);
