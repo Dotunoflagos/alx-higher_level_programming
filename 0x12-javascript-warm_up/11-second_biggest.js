b54ecea8f888814f511ac451f52cea46977062b2
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-24 18:23:29 +0100

    js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const int = process.argv;
+const length = int.length;
+let bigest = Number(int[2]);
+let bigest2nd = Number(int[2]);
+
+if(int == undefined || length < 4) {
+    console.log(0)
+    return
+}
+
+for (let i = 2; i < length; i++) {
+    if (Number(int[i]) > bigest) {
+        bigest = int[i]
+    }
+}
+
+for (let i = 2; i < length; i++) {
+    if (Number(int[i]) < bigest && Number(int[i]) > bigest2nd) {
+        bigest2nd = int[i]
+    }
+}
+
+console.log(bigest2nd);
