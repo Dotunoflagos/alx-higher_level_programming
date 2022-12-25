70b7c1978ff43cf977aa990f90548c7a9b9d9bd5
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-25 17:06:27 +0100

    JS obj

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,51 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      for (let j = this.width; j > 0; j--) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    const temph = this.height;
+    const tempw = this.width;
+    this.height = temph * 2;
+    this.width = tempw * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = this.height; i > 0; i--) {
+        for (let j = this.width; j > 0; j--) {
+          process.stdout.write('C');
+        }
+        console.log('');
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
