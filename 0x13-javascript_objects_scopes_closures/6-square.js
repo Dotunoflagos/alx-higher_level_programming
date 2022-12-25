d365fddc49b208cae0b3bfc56194358f7351b6dd
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-25 17:17:30 +0100

    JS obj

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,34 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = this.height; i > 0; i--) {
-      for (let j = this.width; j > 0; j--) {
-        process.stdout.write('X');
-      }
-      console.log('');
-    }
-  }
-
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-
-  double () {
-    const temph = this.height;
-    const tempw = this.width;
-    this.height = temph * 2;
-    this.width = tempw * 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
