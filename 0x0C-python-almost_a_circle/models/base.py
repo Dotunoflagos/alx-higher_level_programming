bcbe88e61aa20bf26248efc997ac2c26834cff8e
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-10 10:56:41 +0100

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Base model
+"""
+
+
+class Base:
+    """the base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
