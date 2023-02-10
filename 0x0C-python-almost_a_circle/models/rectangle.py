82b27fad9e1c4086fa984fc5457e00c554ee202f
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-10 13:28:19 +0100

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,6 @@ class Rectangle(Base):
             for y in range(self.width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
