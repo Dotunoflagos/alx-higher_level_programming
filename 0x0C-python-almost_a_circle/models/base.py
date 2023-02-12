ffa0b82b20642c9d7abd393c76845f91b6a10bcf
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-12 09:16:59 +0100

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries == None or list_dictionaries == "":
+        if list_dictionaries is None or list_dictionaries == "":
             return "[]"
         else:
             return json.dumps(list_dictionaries)
