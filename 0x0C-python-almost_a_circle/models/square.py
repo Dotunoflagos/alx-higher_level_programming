26ba4e7fe3dad07881be5c7a4405281790131641
dotunoflagos <adedotun_balogun@yahoo.com>
2023-02-12 03:44:03 +0100

    Almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,6 +14,17 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        print(self.__dict__)
+
+    @property
+    def size(self):
+        return self._Rectangle__width
+
+    @size.setter
+    def size(self, size):
+        self.int_validation("width", size)
+        self._Rectangle__width = size
+        self._Rectangle__height = size
 
     def __str__(self):
         """Print Square"""
