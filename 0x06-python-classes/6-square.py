c514ae9d1ff9ffb531507b1af28a1ba479441a62
dotunoflagos <adedotun_balogun@yahoo.com>
2023-03-04 16:12:40 +0100

    Python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,8 +15,10 @@ class Square:
             else:
                 raise TypeError('size must be an integer')
 
-            if not isinstance(position, tuple) \
-                    or isinstance(position[0], int) or isinstance(position[1], int):
+            if not isinstance(position, tuple) or all(isinstance(p, int) for p in position):
+                raise TypeError(
+                    'position must be a tuple of 2 positive integers')
+            elif position[0] is None or position[1] is None:
                 raise TypeError(
                     'position must be a tuple of 2 positive integers')
             elif len(position) < 0 \
