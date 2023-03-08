df38767adc37aa13f4cc415cc31d4691ae9886e9
dotunoflagos <adedotun_balogun@yahoo.com>
2023-03-08 20:54:49 +0100

    Python net

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # Read URL from user input
-
-read -p "Enter URL: " url
-
-# Send request using curl and save response to a variable
-response=$(curl -sI $url)
-
-# Extract the Content-Length header value
-content_length=$(echo "$response" | grep -i content-length | awk '{print $2}' | tr -d '\r')
-
-# Display the size of the body of the response
-echo "${content_length}"
+response=$(curl -sI $1)
+size=$(echo "$response" | awk '/Content-Length/ {print $2}')
+echo "$size"
